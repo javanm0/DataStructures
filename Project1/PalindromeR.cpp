b1bca17bb9bcd8f33c8bb5e654816797b2b0367a
Javan Miller <javan@fastmail.com>
2023-10-07 15:23:40 -0400

    Bug fixing in the recursive test_string function

--- Project1/PalindromeR.cpp
+++ Project1/PalindromeR.cpp
@@ -4,13 +4,7 @@
 #include "PalindromeR.hpp"
 
 int PalindromeR::test_string(const std::string& s)
-{    
-    // String with one character or less is a palindrome
-    if (s.length() <= 1)
-    {
-        return -1;
-    }
-
+{
     // Converts string to lowercase
     std::string lowerS = s;
     for (int i = 0; i < lowerS.length(); i++)
@@ -18,11 +12,40 @@ int PalindromeR::test_string(const std::string& s)
         lowerS[i] = tolower(lowerS[i]);
     }
 
+    // String with one character or less is a palindrome
+    if (s.length() <= 1)
+    {
+        return -1;
+    }
+
+    // Find the index of the first alphabetical character
+    int first = 0;
+    while (isalpha(s[first]) == 0)
+    {
+        first++;
+    }
+
+    // Find the index of the last alphabetical character
+    int last = s.length() - 1;
+    while (isalpha(s[last]) == 0)
+    {
+        last--;
+    }
+
+    // Is palindrome if first is greater than or equal to last char
+    if (first >= last)
+    {
+        return -1;
+    }
+
     // Tests if first and last character in string is equivalent
     // If true, it recursively calls the function by creating a substring
-    if (lowerS[0] == lowerS[lowerS.length() - 1])
+    if (lowerS[first] == lowerS[last])
+    //if (lowerS[0] == lowerS[lowerS.length() - 1])
     {
-        return test_string(lowerS.substr(1, lowerS.length() - 2));    
+        return test_string(lowerS.substr(first + 1, last - first - 1));    
+        // return test_string(lowerS.substr(1, lowerS.length() - 2));    
+
     }
     else
     {
